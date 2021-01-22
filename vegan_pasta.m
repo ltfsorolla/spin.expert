754d25be56243d2994fd4e5c18d8e7ea91e1b845
BoolHandLuke <owens.henry@gmail.com>
2021-01-22 14:26:20 +0000

    Create vegan_pasta.m
    Initial commit

---
+++ vegan_pasta.m
@@ -0,0 +1,13 @@
+title: "vegan pasta"
+
+ingredients:
+- Kosher salt: pinch
+- nutritional yeast: ⅓ cup 
+- cashew butter: ¼ cup
+- white miso paste: 2 tablespoons
+- whole black peppercorns: 2 tablespoons
+- dried spaghetti: 2 tablespoons
+- extra-virgin olive oil: 2 tablespoons
+- lemon: 0.5
+
+This quick veggie lover take on cacio e pepe uses an exemplary method: Cook the pasta barely shy of still somewhat firm, hold a portion of the bland pasta water to add body to the sauce, at that point stew the pasta in its sauce with a sprinkle of pasta water, mixing vivaciously until the sauce is emulsified. While numerous sans dairy pasta plans look to puréed, doused cashews for their smoothness, this one compromises by utilizing locally acquired cashew margarine. A spoonful of miso adds profundity, and tart nourishing yeast adds umami. Toasting the peppercorns supports their flavor and mollifies them.
